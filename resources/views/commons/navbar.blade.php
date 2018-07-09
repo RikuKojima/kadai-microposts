f8a11687b26cabf1d2a455536e5e1fd5cdf2a7db
riku.kojima <rikuthekojima@gmail.com>
2018-07-09 16:11:49 +0000

    user registration

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -17,7 +17,7 @@
             </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#">SignUp</a></li>
+                    <li>{!! link_to_route('signup.get', 'Sign up now!!) !!}</li>
                     <li><a href="#">LogIn</a></li>
                 </ul>
             </div>
